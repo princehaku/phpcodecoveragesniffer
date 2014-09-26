2273e9d354f669abcba7115dda7f3df012a6ab7b
zhongwei.bzw <zhongwei.bzw@taobao.com>
2014-09-26 20:21:31 +0800

    fix path

--- generate.php
+++ generate.php
@@ -10,9 +10,9 @@
 
 include "lib/CodeCovergeSniffer.php";
 // 创建的时候需要指定你的工程目录. 和输出路径
-$working_dir = __DIR__ . "/../../";
-$output_dir =  __DIR__ . "/../../srp/cc/";
+$working_dir = __DIR__ . "/../../../";
+$output_dir =  __DIR__ . "/../../../srp/cc/";
 if (!file_exists($output_dir)) {
     mkdir($output_dir);
 }
-CodeCovergeSniffer::generateHtml("develop", $working_dir, $output_dir);
+CodeCovergeSniffer::generateHtml("mido", $working_dir, $output_dir);
